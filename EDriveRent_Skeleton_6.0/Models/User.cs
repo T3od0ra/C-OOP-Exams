459724fbafd1c5ac8a83ead236dce3145f8107bf
Teodora Petkova <125208300+T3od0ra@users.noreply.github.com>
2024-03-18 16:59:42 +0200

    Add files via upload

---
+++ EDriveRent_Skeleton_6.0/Models/User.cs
@@ -0,0 +1,109 @@
+﻿using EDriveRent.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDriveRent.Models
+{
+    public class User : IUser
+    {
+        private string firstName;
+        private string lastName;
+        private double rating;
+        private string drivingLicenseNumber;
+        private bool isBlocked;
+
+        public User(string firstName, string lastName, string drivingLicenseNumber)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.DrivingLicenseNumber = drivingLicenseNumber;
+        }
+        public string FirstName
+        {
+            get => firstName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("FirstName cannot be null or whitespace!"); 
+                }
+
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("LastName cannot be null or whitespace!");
+                } 
+
+                lastName = value;
+            }
+        }
+
+        public double Rating
+        {
+            get => rating;
+            private set
+            {
+                rating = value;
+            }
+        }
+
+        public string DrivingLicenseNumber
+        {
+            get => drivingLicenseNumber;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Driving license number is required!");
+                }
+
+                drivingLicenseNumber = value;
+            }
+        }
+
+        public bool IsBlocked
+        {
+            get => isBlocked;
+            private set
+            {
+                isBlocked = value;
+            }
+        }
+
+        public void DecreaseRating()
+        {
+            this.Rating -= 2;
+            if (this.Rating < 0)
+            {
+                this.Rating = 0;
+                isBlocked = true;
+            }
+        }
+
+        public void IncreaseRating()
+        {
+            this.Rating += 5;
+            if (this.Rating > 10)
+            {
+               this.Rating = 10;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} Driving license: {drivingLicenseNumber} Rating: {rating}";
+        }
+    }
+}
